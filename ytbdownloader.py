6c4bc4dbeee08d81efc0aa93ec2e3dc0fbc119ce
digal25 <dem25@mail.ru>
2021-11-07 12:09:07 +0300

    Added small changes in ytbdownloader.py

--- ytbdownloader.py
+++ ytbdownloader.py
@@ -54,27 +54,3 @@ def Playlist_downloader(date):
 
 config = open_json()
 download_choicer(config)
-
-# SAVE_PATH = config[0]['tasks'][0]['local_path']
-# print()
-
-# video = YouTube('https://www.youtube.com/watch?v=8eud2ZQHK1s')
-# print('downloading : {} with url : {}'.format(video.title, video.watch_url))
-# video.streams.\
-#     filter(progressive=True, file_extension='mp4').\
-#     order_by('resolution').\
-#     desc().\
-#     first().\
-#     download(SAVE_PATH)
-
-# '''
-# SAVE_PATH = "/Users/galo/Downloads/video/незнайка"
-# playlist = Playlist('https://www.youtube.com/watch?v=fjwF0sCbDtE&list=PL7x1DJTKugPDlSaK0UHPavBovuK8qT38f')
-# for video in playlist.videos:
-#     print('downloading : {} with url : {}'.format(video.title, video.watch_url))
-#     video.streams.\
-#         filter(type='video', progressive=True, file_extension='mp4').\
-#         order_by('resolution').\
-#         desc().\
-#         first().\
-#         download(SAVE_PATH)
