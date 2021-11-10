f45f193467ae2f9525ca2a397e6230cca3169ea3
Galovach Andrey <a.galovach@yandex.ru>
2021-11-10 11:42:24 +0300

    change def get_arg()

--- ytbdownloader.py
+++ ytbdownloader.py
@@ -19,9 +19,6 @@ def get_arg():
 def open_json(args):
 # пытается найти и открыть файл config.json 
 
-    if args.input[-5::] != '.json': #теперь можно не писать расширение *.json (не по ТЗ, но я хотел попробывать.. сработает или нет)
-        args.input += '.json'
-
     try:
         config = json.load(open(args.input))
         return config
